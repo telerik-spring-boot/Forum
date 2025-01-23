d92dd8eb79d8b1210c99c68f15ca4ade643a22b4
Danny <jordantsolov@gmail.com>
2025-01-23 10:14:02 +0200

    create hibernate configuration

---
+++ src/main/java/com/telerik/forum/configurations/HibernateConfiguration.java
@@ -0,0 +1,59 @@
+package com.telerik.forum.configurations;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class HibernateConfiguration {
+
+    private final String dbUrl, dbUsername, dbPassword;
+
+    @Autowired
+    public HibernateConfiguration(Environment env) {
+        dbUrl = env.getProperty("database.url");
+        dbUsername = env.getProperty("database.username");
+        dbPassword = env.getProperty("database.password");
+    }
+
+    @Bean(name = "entityManagerFactory")
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("com.example.telerik.forum");
+        sessionFactory.setHibernateProperties(hibernateProperties());
+
+        return sessionFactory;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName("org.mariadb.jdbc.Driver");
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(dbUsername);
+        dataSource.setPassword(dbPassword);
+
+        return dataSource;
+    }
+
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MariaDBDialect");
+
+        return properties;
+    }
+
+}

d9fc09eee33b902cad45a177b6fb380c19ac738b
Danny <jordantsolov@gmail.com>
2025-01-23 10:47:01 +0200

    create admin pojo model

---
+++ src/main/java/com/telerik/forum/models/Admin.java
@@ -0,0 +1,51 @@
+package com.telerik.forum.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "admins")
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "admin_id")
+    private int id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    public Admin() {}
+
+    public Admin(User user, String phoneNumber) {
+        this.user = user;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}

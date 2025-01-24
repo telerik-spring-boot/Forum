71e1b3b7552bcf2e41f2882db5f3176fbc985101
nPilashev <n.a.pilashev@gmail.com>
2025-01-24 15:46:40 +0000

    Add PostMapper

---
+++ src/main/java/com/telerik/forum/helpers/PostMapper.java
@@ -0,0 +1,37 @@
+package com.telerik.forum.helpers;
+
+import com.telerik.forum.models.Comment;
+import com.telerik.forum.models.Post;
+import com.telerik.forum.models.dtos.CommentDisplayDTO;
+import com.telerik.forum.models.dtos.PostDisplayDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PostMapper {
+
+    public PostDisplayDTO postToPostDisplayDTO(Post post) {
+        PostDisplayDTO postDTO = new PostDisplayDTO();
+
+        postDTO.setTitle(post.getTitle());
+        postDTO.setContent(post.getContent());
+        postDTO.setCreatorUsername(post.getUser().getUsername());
+        List<CommentDisplayDTO> commentDTOS = new ArrayList<>();
+        for (Comment comment : post.getComments()) {
+            commentDTOS.add(commentToCommentDisplayDTO(comment));
+        }
+        postDTO.setComments(commentDTOS);
+
+        return postDTO;
+
+    }
+
+    public CommentDisplayDTO commentToCommentDisplayDTO(Comment comment) {
+        CommentDisplayDTO commentDTO = new CommentDisplayDTO();
+
+        commentDTO.setCommentContent(comment.getContent());
+        commentDTO.setCreatorUsername(comment.getUser().getUsername());
+
+        return commentDTO;
+    }
+}

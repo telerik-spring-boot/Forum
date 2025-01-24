55d19b777a1ff708c30414f66aa08997a3f6077e
Danny <jordantsolov@gmail.com>
2025-01-24 15:05:36 +0200

    create UserCreateDTO

---
+++ src/main/java/com/telerik/forum/models/dtos/userdtos/UserCreateDTO.java
@@ -0,0 +1,80 @@
+package com.telerik.forum.models.dtos.userdtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public class UserCreateDTO {
+
+    @NotBlank
+    @Size(min = 4, max = 32, message = "First name must be between 4 and 32 symbols.")
+    private String firstName;
+
+    @NotBlank
+    @Size(min = 4, max = 32, message = "Last name must be between 4 and 32 symbols.")
+    private String lastName;
+
+    @NotBlank
+    @Size(min = 5, max = 254, message = "Email must be between 5 and 254 symbols.")
+    @Pattern(regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$",
+            message = "Invalid email address")
+    private String emailAddress;
+
+    @NotBlank
+    @Size(min = 4, max = 20, message = "Username must be between 4 and 20 symbols.")
+    private String username;
+
+    @NotBlank
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*(),.?\":{}|<>]).{6,20}$", message = "Password must contain at least one uppercase letter, one lowercase letter, one number, one symbol, and be between 6 to 20 characters long.")
+    private String password;
+
+    public UserCreateDTO() {}
+
+    public UserCreateDTO(String firstName, String lastName, String emailAddress, String username, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailAddress = emailAddress;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}

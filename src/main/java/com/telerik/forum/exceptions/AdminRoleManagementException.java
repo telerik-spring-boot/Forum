512130933ba9adb40a1202bbdcae16bb2055eed1
Danny <jordantsolov@gmail.com>
2025-01-25 16:55:51 +0200

    create AdminRoleManagementException

---
+++ src/main/java/com/telerik/forum/exceptions/AdminRoleManagementException.java
@@ -0,0 +1,7 @@
+package com.telerik.forum.exceptions;
+
+public class AdminRoleManagementException extends RuntimeException {
+    public AdminRoleManagementException(String message) {
+        super(message);
+    }
+}

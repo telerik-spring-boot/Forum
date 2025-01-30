4dd23f3d79608bc358fdf35d7579347a05a12569
Danny <jordantsolov@gmail.com>
2025-01-30 15:06:36 +0200

    commit in the search of the bug 3

--- src/main/java/com/telerik/forum/configurations/PasswordMigrationRunner.java
+++ src/main/java/com/telerik/forum/configurations/PasswordMigrationRunner.java
@@ -9,24 +9,24 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Component
-public class PasswordMigrationRunner implements CommandLineRunner {
-
-    private final UserRepository userRepository;
-
-    public PasswordMigrationRunner(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-    @Override
-    public void run(String... args) {
-        List<User> users = userRepository.getAll(new FilterUserOptions(null, null, null, null,null));
-        for (User user : users) {
-            if (!user.getPassword().startsWith("$2a$")) { // Check if already encoded (BCrypt hashes start with "$2a$")
-
-                user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
-                userRepository.update(user);
-            }
-        }
-    }
-}
+//@Component
+//public class PasswordMigrationRunner implements CommandLineRunner {
+//
+//    private final UserRepository userRepository;
+//
+//    public PasswordMigrationRunner(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
+//
+//    @Override
+//    public void run(String... args) {
+//        List<User> users = userRepository.getAll(new FilterUserOptions(null, null, null, null,null));
+//        for (User user : users) {
+//            if (!user.getPassword().startsWith("$2a$")) { // Check if already encoded (BCrypt hashes start with "$2a$")
+//
+//                user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+//                userRepository.update(user);
+//            }
+//        }
+//    }
+//}

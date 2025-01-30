ed5ab836ec6753c77a9a21962f2b862f0001fad1
Danny <jordantsolov@gmail.com>
2025-01-30 13:52:50 +0200

    exclude datasource autoconfiguration when testing

--- src/test/java/com/telerik/forum/ForumApplicationTests.java
+++ src/test/java/com/telerik/forum/ForumApplicationTests.java
@@ -2,8 +2,12 @@ package com.telerik.forum;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
+})
 class ForumApplicationTests {
 
     @Test

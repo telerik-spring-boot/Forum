688e633f33d5c195178350ebaeb106920b1b6e9a
nPilashev <n.a.pilashev@gmail.com>
2025-02-16 14:30:46 +0000

    Add tags to PostView

--- db/insert-data.sql
+++ db/insert-data.sql
@@ -1,8 +1,12 @@
 INSERT INTO forum_schema.users (first_name, last_name, email_address, username, password)
-VALUES ('John', 'Doe', 'john.doe@example.com', 'johndoe', '$2b$12$ZXIe8X5z8z1YwAvFalNH/Ox8zvkMlrDRfnmUW9azqvsBQ3nBJQ6a.'),
-       ('Jane', 'Smith', 'jane.smith@example.com', 'janesmith', '$2b$12$ZS7DW.KZQH0LhLEOrwlYRuMVhPGtqjFfWJpwQnBVM3qaZ4tHV7C4q'),
-       ('Admin', 'User', 'admin.user@example.com', 'adminuser', '$2b$12$ZJDrZSz/gu1QEd8EADjtn.B1FT78w6LCj2AWWiXjnJGjgvI5eUIJK'),
-       ('Nophone', 'Andy', 'nophone.andy@example.com', 'nophoneadmin', '$2b$12$ZJDrZSz/gu1QEd8EADjtn.B1FT78w6LCj2AWWiXjnJGjgvI5eUIJK');
+VALUES ('John', 'Doe', 'john.doe@example.com', 'johndoe',
+        '$2b$12$ZXIe8X5z8z1YwAvFalNH/Ox8zvkMlrDRfnmUW9azqvsBQ3nBJQ6a.'),
+       ('Jane', 'Smith', 'jane.smith@example.com', 'janesmith',
+        '$2b$12$ZS7DW.KZQH0LhLEOrwlYRuMVhPGtqjFfWJpwQnBVM3qaZ4tHV7C4q'),
+       ('Admin', 'User', 'admin.user@example.com', 'adminuser',
+        '$2b$12$ZJDrZSz/gu1QEd8EADjtn.B1FT78w6LCj2AWWiXjnJGjgvI5eUIJK'),
+       ('Nophone', 'Andy', 'nophone.andy@example.com', 'nophoneadmin',
+        '$2b$12$ZJDrZSz/gu1QEd8EADjtn.B1FT78w6LCj2AWWiXjnJGjgvI5eUIJK');
 
 INSERT INTO forum_schema.posts (title, content, user_id)
 VALUES ('Welcome Post', 'This is the first post in the forum!', 1),
@@ -14,17 +18,24 @@ VALUES ('This is a comment on the first post.', 1, 3),
        ('Comment on the second post.', 2, 4);
 
 INSERT INTO forum_schema.roles(name)
-VALUES('USER'),('ADMIN');
+VALUES ('USER'),
+       ('ADMIN');
 
 INSERT INTO forum_schema.user_roles(user_id, role_id)
-VALUES(1,1),
-      (2,1),
-      (3,1),
-      (3,2),
-      (4,1),
-      (4,2);
+VALUES (1, 1),
+       (2, 1),
+       (3, 1),
+       (3, 2),
+       (4, 1),
+       (4, 2);
 
 INSERT INTO forum_schema.admin_details(user_id, phone_number)
 VALUES (3, '123-456-7890');
+
+INSERT INTO forum_schema.tags(name)
+VALUES ('test');
+
+INSERT INTO forum_schema.post_tags(post_id, tag_id)
+VALUES (1, 1);
 
 

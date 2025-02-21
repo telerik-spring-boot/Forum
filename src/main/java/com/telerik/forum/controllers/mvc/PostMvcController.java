d54408fdf2f9ebac3fccaa45a3fc07e2b2f179a0
nPilashev <n.a.pilashev@gmail.com>
2025-02-21 17:55:59 +0000

    Style changes to single post view to stay consistent with other views

--- src/main/java/com/telerik/forum/controllers/mvc/PostMvcController.java
+++ src/main/java/com/telerik/forum/controllers/mvc/PostMvcController.java
@@ -69,7 +69,8 @@ public class PostMvcController {
 
         if (bindingResult.hasErrors()) {
             extracted(postId, model, user);
-            return "single-post-updated";
+//            return "single-post-updated";
+            return "single-page-updated-thin";
         }
 
         try {
@@ -103,8 +104,9 @@ public class PostMvcController {
             model.addAttribute("commentCreateDto", new CommentCreateDTO());
             model.addAttribute("userId", user.getId());
 
-            return "single-post-updated";
-            //return "single-post";
+            //return "single-post-updated";
+            return "single-page-updated-thin";
+
         } catch (EntityNotFoundException e) {
             model.addAttribute("statusCode", HttpStatus.NOT_FOUND.getReasonPhrase());
             model.addAttribute("error", e.getMessage());

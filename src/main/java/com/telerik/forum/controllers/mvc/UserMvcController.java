978f47dc740dba82888bf64e520779bbe83d05b2
Danny <jordantsolov@gmail.com>
2025-02-23 00:00:00 +0200

    add picture functionality in user view

--- src/main/java/com/telerik/forum/controllers/mvc/UserMvcController.java
+++ src/main/java/com/telerik/forum/controllers/mvc/UserMvcController.java
@@ -74,6 +74,7 @@ public class UserMvcController {
             model.addAttribute("user", showUser);
             model.addAttribute("userId", user.getId());
             model.addAttribute("comment", new CommentCreateDTO());
+            model.addAttribute("profilePicture", FilesHelper.checkIfPhotoExists(id));
             model.addAttribute("currentURI", request.getRequestURI());
 
             return "user";
@@ -99,6 +100,8 @@ public class UserMvcController {
             model.addAttribute("user", showUser);
             model.addAttribute("userId", user.getId());
             model.addAttribute("comment", new CommentCreateDTO());
+            model.addAttribute("currentURI", request.getRequestURI());
+            model.addAttribute("profilePicture", FilesHelper.checkIfPhotoExists(id));
             model.addAttribute("currentURI", request.getRequestURI());
 
             return "user";
@@ -126,6 +129,7 @@ public class UserMvcController {
 
             model.addAttribute("user", new UserCommentsPageDisplayDTO(userEntity.getId(), userEntity.getComments(), userEntity.getUsername()));
             model.addAttribute("userId", user.getId());
+            model.addAttribute("profilePicture", FilesHelper.checkIfPhotoExists(id));
             model.addAttribute("currentURI", request.getRequestURI());
 
             return "user";
